3c465851dd7e174ed876ac3dc7d2cd9d1272f6de
k1000pro <70303778+k1000pro@users.noreply.github.com>
2024-09-12 11:14:17 -0600

    seccion portafolio

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,38 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+const animarQuienesSomos = document.querySelectorAll('.quienes-somos');
+const animarMisionVision = document.querySelectorAll('.mision-vision');
+const seccionObjetivo = document.querySelector('.quienes-somos-section');
+
+// Función que se ejecutará cada vez que se produzca el evento scroll
+function manejarScroll() {
+  // Obtener la posición de la sección objetivo y la ventana
+  console.log(seccionObjetivo.offsetTop)
+  console.log(window.pageYOffset)
+  const posicionSeccion = seccionObjetivo.offsetTop;
+  const posicionVentana = (window.pageYOffset);
+
+  // Si la posición de la ventana es mayor o igual a la de la sección objetivo
+  if (posicionVentana >= posicionSeccion-700) {
+    // Agregar la clase a cada elemento
+    animarQuienesSomos.forEach(elemento => {
+      elemento.classList.add('animate__backInLeft');
+    });
+    animarMisionVision.forEach(elemento => {
+      elemento.classList.add('animate__backInRight');
+    });
+  } else {
+    // Quitar la clase a cada elemento (opcional)
+    animarQuienesSomos.forEach(elemento => {
+      elemento.classList.remove('animate__backInLeft');
+    });
+    animarMisionVision.forEach(elemento => {
+      elemento.classList.remove('animate__backInRight');
+    });
+  }
+  
+}
+
+// Escuchar el evento scroll en la ventana
+window.addEventListener('scroll', manejarScroll);
